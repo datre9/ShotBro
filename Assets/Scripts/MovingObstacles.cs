559352db6ef6b0526eff6e30c33231fa821735fe
datre9 <david.trembacz@gmail.com>
2024-12-11 17:47:41 +0100

    Better moving obstacles with triggers

--- Assets/Scripts/MovingObstacles.cs
+++ Assets/Scripts/MovingObstacles.cs
@@ -4,7 +4,12 @@ using UnityEngine.Tilemaps;
 public class MovingObstacles : MonoBehaviour {
 	private LevelManager levelManager;
 	public Tilemap t;
-	private bool isFalling = false;
+	public bool startMoving = false;
+
+	public float xSpeed;
+	public float ySpeed;
+	public float zSpeed;
+
 
 	void Start() {
 		levelManager = LevelManager.instance;
@@ -12,13 +17,13 @@ public class MovingObstacles : MonoBehaviour {
 
 	private void Update() {
 		if (Input.GetKeyDown(KeyCode.F)) {
-			isFalling = true;
+			startMoving = true;
 		}
-		if (isFalling) {
+		if (startMoving) {
 			t.transform.position = new Vector3(
-				t.transform.position.x,
-				t.transform.position.y - 0.001f,
-				t.transform.position.z);
+				t.transform.position.x + xSpeed,
+				t.transform.position.y + ySpeed,
+				t.transform.position.z + zSpeed);
 		}
 	}
 
