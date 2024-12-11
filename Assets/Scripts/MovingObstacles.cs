c2cd426f88d8e7d3583de21f16fe19c454534695
datre9 <david.trembacz@gmail.com>
2024-12-11 19:24:20 +0100

    Reset moving obstacles after respawn

--- Assets/Scripts/MovingObstacles.cs
+++ Assets/Scripts/MovingObstacles.cs
@@ -10,9 +10,16 @@ public class MovingObstacles : MonoBehaviour {
 	public float ySpeed;
 	public float zSpeed;
 
+	private float x;
+	private float y;
+	private float z;
+
 
 	void Start() {
 		levelManager = LevelManager.instance;
+		x = t.transform.position.x;
+		y = t.transform.position.y;
+		z = t.transform.position.z;
 	}
 
 	private void Update() {
@@ -30,6 +37,9 @@ public class MovingObstacles : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if (collision.gameObject.tag == "Player") {
 			levelManager.RespawnPlayer();
+			//reset position ofter respawn
+			t.transform.position = new Vector3(x, y, z);
+			startMoving = false;
 		}
 	}
 }
